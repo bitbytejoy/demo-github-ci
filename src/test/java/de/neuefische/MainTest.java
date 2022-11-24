c51aae9b1c9590a69f118470225f239c447549ac
bitbytejoy <bitbytejoy@gmail.com>
2022-11-24 13:03:50 +0100

    feat: Intentionally failing test

--- src/test/java/de/neuefische/MainTest.java
+++ src/test/java/de/neuefische/MainTest.java
@@ -1,20 +1,12 @@
 package de.neuefische;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class MainTest {
     @Test
     public void add_when2And5_Then7() {
-        // given
-        int a = 2;
-        int b = 5;
-
-        // when
-        int actual = Main.add(a, b);
-
-        // then
-        assertEquals(9, actual);
+        assertEquals(true, false);
     }
 }
