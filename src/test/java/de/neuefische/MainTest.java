9df1c484ee620acff397beba27606dcf3d7f7690
bitbytejoy <bitbytejoy@gmail.com>
2022-11-24 13:13:15 +0100

    fix: jUnit5 needs Surefire plugin version 2.22.0 min

--- src/test/java/de/neuefische/MainTest.java
+++ src/test/java/de/neuefische/MainTest.java
@@ -1,12 +1,13 @@
 package de.neuefische;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
     @Test
     public void add_when2And5_Then7() {
-        assertEquals(true, false);
+        assertEquals(true, true);
     }
 }
